30c3753201c76e5b2a4051f195c63de1049869f3
Poca23 <cndweb37@gmail.com>
2025-12-15 17:41:34 +0100

    chore(git): configuration .gitignore et template environnement

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,12 @@
+// src/environments/environment.template.ts
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'VOTRE_API_KEY_ICI',
+    authDomain: 'VOTRE_AUTH_DOMAIN_ICI',
+    projectId: 'VOTRE_PROJECT_ID_ICI',
+    storageBucket: 'VOTRE_STORAGE_BUCKET_ICI',
+    messagingSenderId: 'VOTRE_MESSAGING_SENDER_ID_ICI',
+    appId: 'VOTRE_APP_ID_ICI',
+  },
+};
