9e33b74b98772fabd04cc3b5f97f18b98444bd98
Poca23 <cndweb37@gmail.com>
2025-12-15 11:34:42 +0100

    config(env): initialize variables environnement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '...',
-    authDomain: '...',
-    projectId: '...',
-    storageBucket: '...',
-    messagingSenderId: '...',
-    appId: '...',
+    apiKey: 'AIzaSyCCmPkgFKpc3kCseqM2C8YY0U_zhC17yR4',
+    authDomain: 'webisapi-cnd.firebaseapp.com',
+    projectId: 'webisapi-cnd',
+    storageBucket: 'webisapi-cnd.firebasestorage.app',
+    messagingSenderId: '495951405850',
+    appId: '1:495951405850:web:ff43f4db7d28cfe45442a3',
   },
 };
